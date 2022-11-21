9b7c4a2c98719db90f40eb3d5a89c0b191ac1972
KaviyaLabglo <kaviya.labglo@gmail.com>
2022-11-21 12:02:22 +0530

    final commit

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
-from myapp.models import *
+from myapp.models import User_Detail
 
 admin.site.register(User_Detail)
--- myapp/serializers.py
+++ myapp/serializers.py
@@ -1,13 +1,10 @@
 from rest_framework.authtoken.models import Token
-from django.contrib.auth.password_validation import validate_password
 from rest_framework.validators import UniqueValidator
 from rest_framework import serializers
 
 from django.contrib.auth import authenticate
-
+from django.contrib.auth.password_validation import validate_password
 from django.contrib.auth.models import User
-
-from rest_framework import serializers
 
 from myapp.models import User_Detail
 
@@ -82,11 +79,11 @@ class DetailSerializer(serializers.HyperlinkedModelSerializer):
         model = User_Detail
         fields = ["user", "profile_picture", "phone",
                   "about_me",  "date_of_birth",  "gender"]
-        
-        
-    
+
+
 class ChangePasswordSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password = serializers.CharField(
+        write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
     old_password = serializers.CharField(write_only=True, required=True)
 
@@ -96,14 +93,16 @@ class ChangePasswordSerializer(serializers.ModelSerializer):
 
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({"password": "Password fields didn't match."})
+            raise serializers.ValidationError(
+                {"password": "Password fields didn't match."})
 
         return attrs
 
     def validate_old_password(self, value):
         user = self.context['request'].user
         if not user.check_password(value):
-            raise serializers.ValidationError({"old_password": "Old password is not correct"})
+            raise serializers.ValidationError(
+                {"old_password": "Old password is not correct"})
         return value
 
     def update(self, instance, validated_data):
@@ -112,5 +111,3 @@ class ChangePasswordSerializer(serializers.ModelSerializer):
         instance.save()
 
         return instance
-    
-    
